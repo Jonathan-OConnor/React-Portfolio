1a3696d665a6589c64e5876b29a3f56337d193aa
Jonathan O'Connor <JohnPierreOConnor@gmail.com>
2021-05-20 19:29:53 -0400

    implemented mobile view for the about me page

--- client/src/pages/AboutMe.js
+++ client/src/pages/AboutMe.js
@@ -3,31 +3,12 @@ import Grow from '@material-ui/core/Grow';
 import { Redirect } from "react-router-dom"
 import Navbar from "../components/Navbar"
 import DesktopAboutMe from "../components/DesktopAboutMe"
+import MobileAboutMe from "../components/MobileAboutMe"
 
 function AboutMe(props) {
 
 
     const [contactRedirect, setContactRedirect] = useState(false)
-    // const [isMobile, setIsMobile] = useState(false)
-    // const mql = window.matchMedia('(max-width: 600px)')
-    // let mobileView = mql.matches
-
-    // mql.addEventListener('change', (e) => {
-    //     const mobileView = e.matches;
-    //     if (mobileView) {
-    //       setIsMobile(true);
-    //     } else {
-    //       setIsMobile(false);
-    //     }
-    //   });
-
-    // useEffect(() => {
-    //     if(mobileView) {
-    //         setIsMobile(true)
-    //     } else {
-    //         setIsMobile(false)
-    //     }
-    // }, [])
 
     function redirect() {
         setContactRedirect(true)
@@ -39,9 +20,8 @@ function AboutMe(props) {
             {contactRedirect ? <Redirect to='/contact' /> : ''}
             <Grow in={true}>
                 <div>
-                    {props.mobile ? <h1>mobile</h1>: <DesktopAboutMe redirect={redirect} /> }
+                    {props.mobile ? <MobileAboutMe redirect={redirect}/>: <DesktopAboutMe redirect={redirect} /> }
                 </div>
-
             </Grow>
         </div>
     )
