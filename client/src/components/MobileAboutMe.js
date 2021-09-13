28f141cee99846bff71200233566c4e6d34b39d4
Jonathan O'Connor <JohnPierreOConnor@gmail.com>
2021-09-13 16:05:22 -0400

    updated mood tracker description to mention SQL usage

--- client/src/components/MobileAboutMe.js
+++ client/src/components/MobileAboutMe.js
@@ -50,7 +50,7 @@ function MobileAboutMe(props) {
                     
                     <div className="d-block w-100" style={{ position: "relative" }}>
                         <div className="d-flex justify-content-center">
-                            <PortfolioCard title="Daily Mood Tracker Application" description="Mood tracker is an app that allows users to log their emotions on a daily basis. The app stores each entry in association with the date so that the user can easily navigate back to each entry. Their daily emotions are then graphed, converting their daily mood entries into visual data." link="https://mood-notes-tracker.herokuapp.com/index.html" github="https://github.com/Humble-Attackers/Group-Mood-Tracker" image="https://camo.githubusercontent.com/a13ee912cfd63f1de9fcbd8f2a78865c2b24fd599c34c3e1d3cfe3b9989d80db/68747470733a2f2f692e696d6775722e636f6d2f716537667856512e706e67" />
+                            <PortfolioCard title="Daily Mood Tracker Application" description="Mood tracker is an application that allows users to log their emotions on a daily basis. The app stores each entry in association with the date into an SQL database. The user's daily emotions are then graphed, converting their daily mood entries into visual data." link="https://mood-notes-tracker.herokuapp.com/index.html" github="https://github.com/Humble-Attackers/Group-Mood-Tracker" image="https://camo.githubusercontent.com/a13ee912cfd63f1de9fcbd8f2a78865c2b24fd599c34c3e1d3cfe3b9989d80db/68747470733a2f2f692e696d6775722e636f6d2f716537667856512e706e67" />
                         </div>
                     </div>
 
